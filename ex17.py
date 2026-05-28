9bc9c8cc0a8488921a4b898ab79ceb6a63f68eb5
alvesgabriel050-svg <alvesgabriel050@gmail.com>
2026-05-27 21:24:44 -0300

    ex17.py

--- ex17.py
+++ ex17.py
@@ -1,9 +1,8 @@
 temperatura= float(input("Qual a temperatura do dia?"))
 if temperatura >= 0 and temperatura <= 18:
-    print(f"Está FRIO: {temperatura}°")
+    print("Está FRIO: {temperatura}°")
 elif temperatura >= 18 and temperatura <= 30:
     print(f"A temperatura está agradavel:{temperatura}°")
 else:
     print (f"Está Calor:{temperatura}°")
-print ("Alterado por outra pessoa")   
-    
+   
